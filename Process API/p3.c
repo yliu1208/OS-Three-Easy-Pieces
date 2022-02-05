54183ca75ed9bd16cac905d3039a85e002cdd19e
Yang Liu <yangdev1208@gmail.com>
2022-02-05 18:40:58 -0500

    Example of exec() system call

---
+++ Process API/p3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[]) {
+    printf("Hello world (pid:%d)\n", (int) getpid()); 
+    int rc = fork(); 
+    if(rc < 0) {
+        fprintf(stderr, "fork failed\n"); 
+        exit(1); 
+    } else if (rc == 0) {
+        printf("Hello, I am child (pid:%d)\n", (int) getpid()); 
+        char *myargs[3]; 
+        myargs[0] = strdup("wc");     // program: "wc" (word count) 
+        myargs[1] = strdup("p3.c");   // argument: file to count
+        myargs[2] = NULL;             // marks end of array
+        execvp(myargs[0], myargs);    // runs word count
+        printf("This shouldn't print out"); 
+    } else {
+        int rc_wait = wait(NULL); 
+        printf("Hello, I am parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid()); 
+    } 
+    return 0; 
+}
